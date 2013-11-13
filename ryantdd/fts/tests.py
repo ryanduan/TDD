a675e889858fbd63f4c7f5c38bf056e020c073db
ryanduan <ryan@deliveryherochina.com>
2013-11-13 16:49:35 +0800

    Part 1: FT to login and look for Polls

--- ryantdd/fts/tests.py
+++ ryantdd/fts/tests.py
@@ -1,5 +1,6 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class PollsTest(LiveServerTestCase):
 
@@ -18,6 +19,22 @@ class PollsTest(LiveServerTestCase):
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Django administration', body.text)
 
-        # TODO: use the admin site to create a Poll
-        self.fail('finish this test')
-        
+        # She types in her username and passwords and hits return
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('ryan')
+
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('123qwe')
+        password_field.send_keys(Keys.RETURN)
+
+        # her username and password are accepted, and she is taken to
+        # the Site Administration page
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+
+        # She now sees a couple of hyperlink that says "Polls"
+        polls_links = self.browser.find_elements_by_link_text('Polls')
+        self.assertEquals(len(polls_links), 2)
+
+        # TODO: Gertrude uses the admin site to create a new Poll
+        self.fail('todo: finish tests')
