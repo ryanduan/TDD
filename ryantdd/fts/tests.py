fb940a283f0dd3b3793d4fa2e728aa903684385e
ryanduan <ryan@deliveryherochina.com>
2013-11-13 17:01:40 +0800

    Finished Part 1

--- ryantdd/fts/tests.py
+++ ryantdd/fts/tests.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class PollsTest(LiveServerTestCase):
-
+    fixtures = ['admin_user.json']
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
